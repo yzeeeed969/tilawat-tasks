5ba920f1997623305bf799bf131f9de8e748a9d3
يزيد يزيد <yzydyzyd@MacBook-Pro-yzyd.local>
2026-06-04 12:04:21 +0300

    bump frontend cache version

--- artifacts/tilawat-tasks/public/sw.js
+++ artifacts/tilawat-tasks/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tilawat-v15";
+const CACHE_NAME = "tilawat-v16";
 const STATIC_ASSETS = ["/manifest.json", "/favicon.svg", "/logo.svg"];
 
 self.addEventListener("install", (event) => {
